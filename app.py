9688838da87437f780e08f326591c20dbbec7f0b
Apprentice-doa <akdaniel0009@gmail.com>
2023-08-04 05:54:07 +0100

    six

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from PIL import Image
 
 @st.cache_data
 def load (link):
-    #data = pd.read_csv('train.csv', nrows=nrows)
     url = link
     conn = st.experimental_connection("gsheets", type=GSheetsConnection)
     data = conn.read(spreadsheet=url)
@@ -23,17 +22,10 @@ ld = load("https://docs.google.com/spreadsheets/d/1Q3JpZ76p3_B2kWdP23373GzhpJ9kQ
 image = Image.open('cart.png')
 
 st.image(image, width=100)
-
-
 st.header("E-Commerce Data Analysis and  Visualization")
-#st.bar_chart(ld["Product"])
 st.dataframe(ld)
 
 fig = plt.figure(figsize=(10, 10))
 sns.countplot(x= "Product", data=ld)
 st.pyplot(fig)
 
-
-
-#st.title("E-Commerce Data Visualization")
-#st.dataframe(data)
