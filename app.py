5cf847da01093a4fa221a93e713f1d2a32b3bcf1
Apprentice-doa <akdaniel0009@gmail.com>
2023-08-04 04:09:47 +0100

    one

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+#import numpy as np
+import plotly.figure_factory as ff
+#import matplotlib.pyplot as plt
+import seaborn as sns
+
+@st.cache_data
+def load (link):
+    #data = pd.read_csv('train.csv', nrows=nrows)
+    url = link
+    conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+    data = conn.read(spreadsheet=url)
+    return data
+
+ld = load("https://docs.google.com/spreadsheets/d/1Q3JpZ76p3_B2kWdP23373GzhpJ9kQhqGNvuad-6fHUc/edit?usp=sharing")
+
+
+st.title("E-Commerce Data Visualization")
+#st.bar_chart(ld["Product"])
+st.dataframe(ld)
+
+#fig = plt.figure(figsize=(10, 10))
+sns.countplot(x= "Product", data=ld)
+#st.pyplot(fig)
+
+
+
+#st.title("E-Commerce Data Visualization")
+#st.dataframe(data)
